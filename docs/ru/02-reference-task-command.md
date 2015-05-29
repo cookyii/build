ec37685ab2342b577f9606113f2adcef3be18262
Revin Roman <xgismox@gmail.com>
2015-05-29 16:25:02 +0300

    Update

--- docs/ru/02-reference-task-command.md
+++ docs/ru/02-reference-task-command.md
@@ -1,6 +1,19 @@
 CommandTask
 ===========
 
+**\cookyii\build\tasks\CommandTask**
+
 задача выполняет произвольную программу в командной строке (cli command).
 
-WIP
+Reference
+---------
+
+Класс `CommandTask` наследует все атрибуты и методы класса [`AbstractTask`][], а также имеет ряд собственных атрибутов.
+
+| Атрибут | Тип | Описание | 
+| ------- | --- | -------- |
+| `commandline` | `string|array` | Одна команда или массик команд, которые необходимо выполнить в cli. |
+| `cwd` | `string|null` | Путь директории, в которой необходимо выполнить команду. |
+| `callback` | `callable` | Анонимная функция или имя функции, заданное строковой переменной или массивом (например: `functionname`, `[$SomeObject, 'MethodName']`, `function(CommandTask $Task, $result){}`), которую необходимо выполнить после отработки программы. |
+
+[`AbstractTask`]: 02-reference-abstract-task.md
--- tasks/CommandTask.php
+++ tasks/CommandTask.php
@@ -19,9 +19,6 @@ class CommandTask extends AbstractTask
     /** @var string|null */
     public $cwd;
 
-    /** @var array|null */
-    public $env;
-
     /** @var callable|null */
     public $callback;
 
@@ -33,7 +30,11 @@ class CommandTask extends AbstractTask
         if (empty($this->commandline)) {
             throw new \InvalidArgumentException('Empty command.');
         } else {
-            $basePath = $this->command->configReader->basePath;
+            $cwd = $this->command->configReader->basePath;
+
+            if (!empty($this->cwd)) {
+                $cwd = $this->cwd;
+            }
 
             if (is_string($this->commandline)) {
                 $this->commandline = [$this->commandline];
@@ -42,15 +43,19 @@ class CommandTask extends AbstractTask
             foreach ($this->commandline as $command) {
                 if ($this->output->isVerbose()) {
                     $this->log(sprintf('Executing "%s"', $command));
-                    $this->log(sprintf('in "%s"', $basePath));
+                    $this->log(sprintf('in "%s"', $cwd));
                 }
 
                 $commands = [
-                    sprintf('cd %s', escapeshellarg($basePath)),
+                    sprintf('cd %s', escapeshellarg($cwd)),
                     $command,
                 ];
 
                 passthru(implode(' && ', $commands), $return);
+
+                if (is_callable($this->callback)) {
+                    call_user_func($this->callback, $this, $return);
+                }
 
                 if ($return !== 0) {
                     return false;
